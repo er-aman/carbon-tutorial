33283bf5d752bb70d8be4dbfff13d308a29d2645
Aman Arora <amanarora@Amans-MBP.attlocal.net>
2020-09-08 23:43:55 -0700

    feat(tutorial): complete step 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import App from './App';
 import { shallow } from 'enzyme';
-
 describe('React Step 1 Tests', () => {
   it('renders without crashing', () => {
     shallow(<App />);
